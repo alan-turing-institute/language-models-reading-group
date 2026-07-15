453b26231b202ef731f7867871548d4679034626
James Geddes <geddes.james@me.com>
2026-07-15 10:13:31 +0100

    Working on racket

--- implementations/bengio2003/racket/learn.rkt
+++ implementations/bengio2003/racket/learn.rkt
@@ -1,13 +1,42 @@
 #lang racket/base
 
+(require math/array)
+
+
 #|
+
 Bengio 2003
 
-N : context length (in the paper, this is n - 1)
+nC : context length (in the paper, the context length is n - 1)
+dV : vocabulary size
+dE : dimension of the embedding space
+dH : dimension of the "hidden layer"
 
+Model parameters
+----------------
 
-The probability of word i given the previous 
+Y = b + W Cs + U tanh(d + H Cs) 
+probs = softmax(Y)
 
+C : An array of shape #(dV dE)
+for each word, a vector of length dE representing that word's embedding
+
+Cs : An array of shape #(nC dE)
+for each word in the context, that word's representation from C
+
+W : An array of shape #(dV (* nC dE))  
+b : A vector of length dV
+
+H : An array of shape #(dH (* nC dE))
+d : A vector of length dH
+U : An array of shape #(dV dH)
 
 |#
+
+
+(define (probabilities C H d W U b words)
+  
+
+  )
+
 
